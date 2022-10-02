f9179a874bb19760f20ba012154d62f89cfd1081
Jan Šmikmátor <smikmatorj.04@spst.eu>
2022-10-02 19:45:28 +0200

    variables

--- WindowsFormsApp7/WindowsFormsApp7/Level_1.Designer.cs
+++ WindowsFormsApp7/WindowsFormsApp7/Level_1.Designer.cs
@@ -28,32 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.btnrestart = new System.Windows.Forms.Button();
+            this.lblstatus = new System.Windows.Forms.Label();
+            this.lbltimeleft = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // button1
+            // btnrestart
             // 
-            this.button1.Location = new System.Drawing.Point(459, 149);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(122, 84);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnrestart.Location = new System.Drawing.Point(628, 12);
+            this.btnrestart.Name = "btnrestart";
+            this.btnrestart.Size = new System.Drawing.Size(205, 84);
+            this.btnrestart.TabIndex = 0;
+            this.btnrestart.Text = "button1";
+            this.btnrestart.UseVisualStyleBackColor = true;
+            this.btnrestart.Click += new System.EventHandler(this.RestartGameEvent);
+            // 
+            // lblstatus
+            // 
+            this.lblstatus.AutoSize = true;
+            this.lblstatus.Location = new System.Drawing.Point(625, 148);
+            this.lblstatus.Name = "lblstatus";
+            this.lblstatus.Size = new System.Drawing.Size(118, 16);
+            this.lblstatus.TabIndex = 1;
+            this.lblstatus.Text = "Match or Mismatch";
+            // 
+            // lbltimeleft
+            // 
+            this.lbltimeleft.AutoSize = true;
+            this.lbltimeleft.Location = new System.Drawing.Point(625, 201);
+            this.lbltimeleft.Name = "lbltimeleft";
+            this.lbltimeleft.Size = new System.Drawing.Size(81, 16);
+            this.lbltimeleft.TabIndex = 2;
+            this.lbltimeleft.Text = "Time left : 30";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.TimerEvent);
             // 
             // Level_1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbltimeleft);
+            this.Controls.Add(this.lblstatus);
+            this.Controls.Add(this.btnrestart);
             this.Name = "Level_1";
             this.Text = "Level_1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnrestart;
+        private System.Windows.Forms.Label lblstatus;
+        private System.Windows.Forms.Label lbltimeleft;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- WindowsFormsApp7/WindowsFormsApp7/Level_1.cs
+++ WindowsFormsApp7/WindowsFormsApp7/Level_1.cs
@@ -12,9 +12,44 @@ namespace WindowsFormsApp7
 {
     public partial class Level_1 : Form
     {
+        List<int> numbers = new List<int> { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6 };
+        string firstChoice;
+        string secondChoice;
+        int tries;
+        List<PictureBox> pictures = new List<PictureBox>();
+        PictureBox picA;
+        PictureBox picB;
+        int totalTime = 30;
+        int countDownTime;
+        bool gameOver = false;
         public Level_1()
         {
             InitializeComponent();
+        }
+
+        private void TimerEvent(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RestartGameEvent(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadPictures()
+        {
+
+        }
+
+        private void CheckPictures(PictureBox A, PictureBox B)
+        {
+
+        }
+
+        private void GameOver()
+        {
+
         }
     }
 }
